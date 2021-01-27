77f17238c61a8a90c9fa2908fbe552d079a49f6c
Thomas Barregren <thomas@kntnt.com>
2021-01-27 19:43:09 +0100

    Create site-specific-code.php

---
+++ site-specific-code.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Site Specific Code
+ * Plugin URI:        https://www.kntnt.com/
+ * Description:       Provides functionality similar to functions.php. 
+ * Version:           1.0.0
+ * Author:            Thomas Barregren
+ * Author URI:        https://www.kntnt.com/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+
+namespace Kntnt\Site_Specific_Code;
+
+defined( 'ABSPATH' ) || die;
+
+/* Put you code below. Notice that this code runs before plugins and theme are
+ * loaded. Use proper hooks for your code. Use for instance the actions
+ * 'setup_theme' and 'after_setup_theme' to execute code just before and after
+ *  respectively, the functions.php of the active theme.
+ */
+
